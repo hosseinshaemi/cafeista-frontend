df9cb9382b5557a8c2a0f5ead9f6d2e40d66ec6c
Negar Amini <Negaaraminii@gmail.com>
2023-04-10 19:22:31 +0430

    User Signup

---
+++ user/src/pags/Verification2/CustomInput2.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./CustomInput2.css";
+
+function VerificationCode({ verificationCode, setVerificationCode }) {
+
+  const handleInput = (index, e) => {
+    const value = e.target.value;
+    setVerificationCode((prevState) => {
+      const newState = [...prevState];
+      newState[index] = value;
+      return newState;
+    });
+    if (value && index < verificationCode.length - 1) {
+      document.querySelectorAll("input")[index + 1].focus();
+    }
+  };
+
+  return (
+    <div className="verification-code-container">
+      <div className="verification-code-inputs">
+        {verificationCode.map((digit, index) => (
+          <input
+            key={index}
+            type="text"
+            maxLength="1"
+            value={digit}
+            onChange={(e) => handleInput(index, e)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default VerificationCode;
